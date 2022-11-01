8f1593256a37f76a48fa7fa0a11d0e07ce309411
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2022-11-01 14:21:09 +0000

    Add simple form

---
+++ src/myForm.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+export default function MyForm() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+  const [fname, setFname] = useState('');
+  const [lname, setLname] = useState('');
+
+  return (
+    <div>
+
+      <hr />
+      {fname} <br />
+      {lname} <br />
+      <hr/>
+
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+
+      <form action="/action_page.php">
+        <label for="fname">First name:</label><br />
+        <input type="text" id="fname" name="fname" value={fname} onChange={event => setFname(event.target.value)}/><br />
+        <label for="lname">Last name:</label><br />
+          <input type="text" id="lname" name="lname" value={lname}  onChange={event => setLname(event.target.value)}/><br /><br />
+          <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
