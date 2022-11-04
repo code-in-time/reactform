1894b70f9beca8bd4d6264a54aee4b7f0ce3967a
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2022-11-04 10:11:57 +0000

    Save

---
+++ src/TestMobx.tsx
@@ -0,0 +1,20 @@
+// import { Routes, Route, Link } from "react-router-dom";
+
+function TextMobx(): JSX.Element {
+
+  // const newCount = () => {
+  //   let newCount = mainCounter + 1; 
+  //   setMainCounter(newCount); 
+  //   console.log();
+  // }
+
+  return (
+    <div className="TextMobx">
+      {/* Home maincounter = {mainCounter}
+      <br /><br />
+      <button type="button" class="btn btn-primary" onClick={() => newCount()}>add button</button> */}
+    </div>
+  );
+}
+
+export default TextMobx;
