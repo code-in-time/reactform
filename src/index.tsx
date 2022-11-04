f97f0362113cb1a46013b6ddcdc08a79b684779e
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2022-11-04 10:24:10 +0000

    Convert to type script

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,11 @@ import reportWebVitals from './reportWebVitals';
 import './custom.scss';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
