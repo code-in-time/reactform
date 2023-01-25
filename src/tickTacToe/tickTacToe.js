4667a36702c9a79d33fc721b7c48c9502df9bc0c
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2023-01-25 13:27:46 +0000

    Add gridg table

--- src/tickTacToe/tickTacToe.js
+++ src/tickTacToe/tickTacToe.js
@@ -4,6 +4,17 @@ function TicTacToe() {
   return (
     <div className="TicTacToe">
       TicTacToe
+      <div class="grid-container">
+        <div class="grid-item">1</div>
+        <div class="grid-item">2</div>
+        <div class="grid-item">3</div>
+        <div class="grid-item">4</div>
+        <div class="grid-item">5</div>
+        <div class="grid-item">6</div>
+        <div class="grid-item">7</div>
+        <div class="grid-item">8</div>
+        <div class="grid-item">9</div>
+      </div>
     </div>
   );
 }
