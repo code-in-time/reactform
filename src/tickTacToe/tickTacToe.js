c14d82d89ed00e01d56a81be5444f9a2894f8b71
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2023-01-26 17:52:17 +0000

    Add winner checks

--- src/tickTacToe/tickTacToe.js
+++ src/tickTacToe/tickTacToe.js
@@ -2,29 +2,56 @@ import GameIcon from './gameIcon'
 // import { Routes, Route, Link } from "react-router-dom";
 import React, { useState, useEffect } from 'react';
 
+const winning = [
+  [1, 2, 3],
+  [4, 5.6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7]];
+
 function TicTacToe() {
   const [playerTurn, setPlayerTurn] = useState(0);
   const [clickedBlocksHuman, setClickedBlocksHuman] = useState([]);
   const [clickedBlocksComputer, setClickedBlocksComputer] = useState([]);
+  const [winner, setWinner] = useState(null);
+
+  const checkForWinner = () => {
+
+    for (let i = 0; i < winning.length; i++) {
+      const currItem = winning[i];
+      console.log(currItem);
+      // check human
+      if (currItem.every(r => clickedBlocksHuman.includes(r))) {
+        console.log('Found all of', currItem, 'in', clickedBlocksHuman);
+        setWinner(0);
+        break;
+      }
+
+      // check computer
+      if (currItem.every(r => clickedBlocksComputer.includes(r))) {
+        console.log('Found all of', currItem, 'in', clickedBlocksComputer);
+        setWinner(1);
+        break;
+      }
+
+    }
+
+  }
+
+  
 
   useEffect(() => {
-    console.log(convertPlayerToText(0), clickedBlocksHuman);
-    console.log(convertPlayerToText(1), clickedBlocksComputer);
-  }, [playerTurn, clickedBlocksHuman, clickedBlocksComputer]);
+    checkForWinner();
+  }, [clickedBlocksHuman, clickedBlocksComputer]);
 
 
   const convertPlayerToText = (playerTurn) => playerTurn === 0 ? 'human' : 'computer';
-  const winning = [
-    [1, 2, 3],
-    [4, 5.6],
-    [7, 8, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7]]
 
-  const disableButton = (id) => clickedBlocksHuman.includes(id) || clickedBlocksComputer.includes(id);
+
+  const disableButton = (id) => clickedBlocksHuman.includes(id) || clickedBlocksComputer.includes(id) || winner !== null;
 
   const showIcon = (id) => {
     if (clickedBlocksHuman.includes(id)) {
@@ -52,10 +79,9 @@ function TicTacToe() {
 
   return (
     <div className="TicTacToe">
-      {clickedBlocksComputer}
-      turn: {convertPlayerToText(playerTurn)}
-      <br />
-      TicTacToe  <GameIcon icon='x' /> <GameIcon icon='o' />
+      {winner !== null && `THE WINNER IS: ${convertPlayerToText(winner)}`}
+       <hr />
+      {winner === null && `whos turn: ${convertPlayerToText(playerTurn)}`}
       <div className="grid-container">
         <button onClick={() => clickBox(1)} disabled={disableButton(1)} className="grid-item">{showIcon(1)}</button>
         <button onClick={() => clickBox(2)} disabled={disableButton(2)} className="grid-item">{showIcon(2)}</button>
